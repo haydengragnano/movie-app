4ef4ace0770fe5385ba7ae5a84888913706bdc00
hayden <1haydengragnano@gmail.com>
2021-08-28 20:38:51 -0700

    adds homework

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,8 +1,16 @@
 class ActorsController < ApplicationController
   def actor 
     actor = Actor.find(1)
-
     render json: {message: actor}
   end 
+
+  def single_actor
+    user_input = params["actor"]
+    render json: user_input.as_json
+  end
   
+  def single_actor_url
+    user_input = params[:wildcard]
+    render json: user_input.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
 
   get "/all_movies" , controller: "movies", action: "all_movies"
 
+  get "/single_actor" , controller: "actors", action:"single_actor"
+
+  get get "/single_actor_url/:wildcard" , controller: "actors", action:"single_actor"
+
+  post "/single_actor" , controller: "actors", action:"single_actor"
+
 end
