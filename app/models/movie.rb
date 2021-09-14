776c62bcd44d933433ec3d2e7e2c4f7984717255
hayden <1haydengragnano@gmail.com>
2021-09-14 15:09:15 -0700

    adds homework

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
+
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   has_many :actors
   
   validates :title, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,23 @@
 
 # movie = Movie.create(title:"step brothers", year: 2014, plot:"a family becomes conjoined with there 40 year old sons who do not know how to get along")
 
-actor = Actor.new (first_name: "Keanu", last_name: "Reeves",known_for: "The matrix", age: 57, gender: "male", movie_id: 2) actor.save
+#actor = Actor.new (first_name: "Keanu", last_name: "Reeves",known_for: "The matrix", age: 57, gender: "male", movie_id: 2) actor.save
+
+# Genre.create!([
+#   {name: "action"},
+#   {name: "comedy"},
+#   {name: "thriller"},
+#   {name: "romance"}
+# ])
+
+MovieGenre.create!([
+  {movie_id: 1, genre_id: 1},
+  {movie_id: 1, genre_id: 2},
+  {movie_id: 1, genre_id: 3},
+  {movie_id: 2, genre_id: 1},
+  {movie_id: 2, genre_id: 3},
+  {movie_id: 2, genre_id: 4},
+  {movie_id: 3, genre_id: 1},
+  {movie_id: 3, genre_id: 3},
+  {movie_id: 3, genre_id: 4},
+])
